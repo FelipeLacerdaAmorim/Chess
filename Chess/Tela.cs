287a43dbc3f20a6662b66e2839613b4864f13895
Felipe Lacerda Amorim <joaofelipeamorim2001@gmail.com>
2021-08-24 00:06:15 -0300

    Adicionando os possiveis movimentos das peças

--- Chess/Tela.cs
+++ Chess/Tela.cs
@@ -10,20 +10,38 @@ namespace Chess
     {
         public static void imprimirTabuleiro(Tabuleiro tab)
         {
-            for(int i = 0; i < tab.Linhas; i++)
+            for (int i = 0; i < tab.Linhas; i++)
             {
                 Console.Write(8 - i + " "); //Mostrar o número das linhas 
-                for(int j = 0; j < tab.Colunas; j++)
+                for (int j = 0; j < tab.Colunas; j++)
                 {
-                    if(tab.peca(i, j) == null)
+                    Tela.imprimirPeca(tab.peca(i, j));
+
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("  a b c d e f g h");
+        }
+        public static void imprimirTabuleiro(Tabuleiro tab, bool[,] posicoesPossiveis)
+        {
+            ConsoleColor fundoOriginal = Console.BackgroundColor;
+            ConsoleColor fundoAlterado = ConsoleColor.DarkGray;
+
+            for (int i = 0; i < tab.Linhas; i++)
+            {
+                Console.Write(8 - i + " "); //Mostrar o número das linhas 
+                for (int j = 0; j < tab.Colunas; j++)
+                {
+                    if (posicoesPossiveis[i, j])
                     {
-                        Console.Write("- ");
+                        Console.BackgroundColor = fundoAlterado;
                     }
                     else
                     {
-                        Tela.imprimirPeca(tab.peca(i, j));
-                        Console.Write(" ");
+                        Console.BackgroundColor = fundoOriginal;
                     }
+                    Tela.imprimirPeca(tab.peca(i, j));
+                    Console.BackgroundColor = fundoOriginal;
                 }
                 Console.WriteLine();
             }
@@ -31,16 +49,26 @@ namespace Chess
         }
         public static void imprimirPeca(Peca peca)
         {
-            if(peca.Cor == Cor.branco)
+
+            if (peca == null)
             {
-                Console.Write(peca);
+                Console.Write("- ");
             }
             else
             {
-                ConsoleColor aux = Console.ForegroundColor;
-                Console.ForegroundColor = ConsoleColor.Cyan;
-                Console.Write(peca);
-                Console.ForegroundColor = aux;
+
+                if (peca.Cor == Cor.branco)
+                {
+                    Console.Write(peca);
+                }
+                else
+                {
+                    ConsoleColor aux = Console.ForegroundColor;
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.Write(peca);
+                    Console.ForegroundColor = aux;
+                }
+                Console.Write(" ");
             }
         }
 
